647f8bb98dcf693c9511cd194ed6e60a415d9ad7
Roman Linkov <roman.linkov@vk.com>
2023-03-06 19:38:09 +0300

    Выполнена задача 47

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -4,6 +4,9 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+// Console.WriteLine($"{twoDimIntArray[0,0] }");
+// string[,] table=new string[m,n]; Задавание массива строк
+
 Console.Clear();
 Console.WriteLine("Здравствуйте! Спасибо, что проверяете моё дз и особенно большое спасибо за комментарии!");
 
@@ -13,48 +16,39 @@ int m=NumberInput("Введите количество столбцов двум
 int min=NumberInput("Введите максимальный элемент массива: ");
 int max=NumberInput("Введите минимальный элемент массива: ");
 
-double[,] twoDimDoubleArray = new double[n,m]; // задавание массива целых чисел
+int[,] twoDimIntArray = new int[n,m]; // задавание массива целых чисел
 
-FillTwoDimDoubleArray(twoDimDoubleArray, min, max);
+FillTwoDimIntArray(twoDimIntArray, min, max);
 
-PrintTwoDimDoubleArray(twoDimDoubleArray);
+PrintTwoDimIntArray(twoDimIntArray);
 
-void FillTwoDimDoubleArray(double[,] twoDimDoubleArray2, int min, int max)
+void FillTwoDimIntArray(int[,] TwoDimIntArray2, int min, int max)
 {
-    int rows1=twoDimDoubleArray2.GetLength(0); // число строк
-    int columns1=twoDimDoubleArray2.GetLength(1); // число столбцов
-
-    Random rand = new Random();
+    int rows1=TwoDimIntArray2.GetLength(0); // число строк
+    int columns1=TwoDimIntArray2.GetLength(1); // число столбцов
 
     for (int i = 0; i < rows1; i++)
     {
         for (int j = 0; j < columns1; j++)
         {
-            double value = rand.NextDouble()*(max-min)+min;
-            twoDimDoubleArray2[i,j]=value;
+            TwoDimIntArray2[i,j]=new Random().Next(min,max+1);
         }
     }
 }
 
-void PrintTwoDimDoubleArray(double[,] twoDimDoubleArray1)
+void PrintTwoDimIntArray(int[,] twoDimIntArray1)
 {
-    int rows=twoDimDoubleArray1.GetLength(0); // число строк
-    int columns=twoDimDoubleArray1.GetLength(1); // число столбцов
+    int rows=twoDimIntArray1.GetLength(0); // число строк
+    int columns=twoDimIntArray1.GetLength(1); // число столбцов
 
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
         {
-            Console.Write($"{Math.Round(twoDimDoubleArray1[i,j],1)} ");
+            Console.Write($"{twoDimIntArray1[i,j]} ");
         }
         Console.WriteLine();
     }
 }
 
 int NumberInput(string msg)
-{
-    System.Console.Write(msg);
-    string value = Console.ReadLine();
-    int result = Convert.ToInt32(value);
-    return result;
-}
